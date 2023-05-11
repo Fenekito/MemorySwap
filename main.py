1102f0ef4beba7d74fe45dbe3fa3689bd8cd6c9a
Fenekito <58893840+Fenekito@users.noreply.github.com>
2023-05-11 20:26:11 -0300

    Change to Dict

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from colorama import Fore, init
 from time import sleep
 
+
 def __main__():
     init()
-    print("            _____    _____   _   _   ____  _______  _    _  _____   _   _   ____    ____    __     _____  _____  _____  _       ____  ")
+    print(
+        "            _____    _____   _   _   ____  _______  _    _  _____   _   _   ____    ____    __     _____  _____  _____  _       ____  ")
     sleep(0.1)
-    print("     /\    |  __ \  / ____| | \ | | / __ \|__   __|| |  | ||  __ \ | \ | | / __ \  |___ \  /  \   / ____||_   _|/ ____|| |     / __ \ ")
+    print(
+        "     /\    |  __ \  / ____| | \ | | / __ \|__   __|| |  | ||  __ \ | \ | | / __ \  |___ \  /  \   / ____||_   _|/ ____|| |     / __ \ ")
     sleep(0.1)
-    print("    /  \   | |  | || (___   |  \| || |  | |  | |   | |  | || |__) ||  \| || |  | |   __) || () | | |       | | | |     | |    | |  | |")
+    print(
+        "    /  \   | |  | || (___   |  \| || |  | |  | |   | |  | || |__) ||  \| || |  | |   __) || () | | |       | | | |     | |    | |  | |")
     sleep(0.1)
-    print("   / /\ \  | |  | | \___ \  | . ` || |  | |  | |   | |  | ||  _  / | . ` || |  | |  |__ <  \__/  | |       | | | |     | |    | |  | |")
+    print(
+        "   / /\ \  | |  | | \___ \  | . ` || |  | |  | |   | |  | ||  _  / | . ` || |  | |  |__ <  \__/  | |       | | | |     | |    | |  | |")
     sleep(0.1)
-    print("  / ____ \ | |__| | ____) | | |\  || |__| |  | |   | |__| || | \ \ | |\  || |__| |  ___) |       | |____  _| |_| |____ | |____| |__| |")
+    print(
+        "  / ____ \ | |__| | ____) | | |\  || |__| |  | |   | |__| || | \ \ | |\  || |__| |  ___) |       | |____  _| |_| |____ | |____| |__| |")
     sleep(0.1)
-    print(" /_/    \_\|_____/ |_____/  |_| \_| \____/   |_|    \____/ |_|  \_\|_| \_| \____/  |____/         \_____||_____|\_____||______|\____/ ")
+    print(
+        " /_/    \_\|_____/ |_____/  |_| \_| \____/   |_|    \____/ |_|  \_\|_| \_| \____/  |____/         \_____||_____|\_____||______|\____/ ")
     sleep(0.1)
     print(f"{Fore.WHITE}┌──────────────────────────────────────────────────────────────────────────────┐")
     print(f"{Fore.WHITE}│                                                                              │")
@@ -21,11 +28,11 @@ def __main__():
     print(f"{Fore.WHITE}│                                                                              │")
     print(f"{Fore.WHITE}└──────────────────────────────────────────────────────────────────────────────┘")
     x = input()
-    nums = list()
-    positions = list()
-    while(x != "c"):
+    partitions = list()
+    values = list()
+    while (x != "c"):
         if x.isdigit():
-            nums.append(int(x))
+            partitions.append({"Size" : int(x), "Occupied" : False, "Value" : 0})
         x = input()
     x = ""
     choices = ["best", "worst", "first"]
@@ -34,14 +41,15 @@ def __main__():
     print(f"{Fore.GREEN}│Digite um número para usar como valores para inserir na partição ou digite c para prosseguir│")
     print(f"{Fore.WHITE}│                                                                                            │")
     print(f"{Fore.WHITE}└────────────────────────────────────────────────────────────────────────────────────────────┘")
-    
-    while(x != "c"):
+
+    while (x != "c"):
         if x.isdigit():
-            positions.append(int(x))
+            values.append(int(x))
         x = input()
     x = ""
 
-    while(x.lower() not in choices):
+
+    while (x.lower() not in choices):
         print(f"{Fore.WHITE}┌────────────────────────────────────────────────┐")
         print(f"{Fore.WHITE}│                                                │")
         print(f"{Fore.GREEN}│Escolha entre o best-fit, worst-fit, e first-fit│")
@@ -51,62 +59,58 @@ def __main__():
     result = x.lower()
     match result:
         case "best":
-            swapBest(nums, positions)
+            fitBest(partitions, values)
         case "worst":
-            swapWorst(nums, positions)
+            fitWorst(partitions, values)
         case "first":
-            swapFirst(nums, positions)
+            fitFirst(partitions, values)
 
 
-def swapFirst(arr, pos):
+def fitFirst(parts, vals):
+    for part, val in zip(parts, vals):
+        if not part["Occupied"]:
+            part["Occupied"] = True
+            part["Value"] = val
+
+    for part in parts:
+        print(f"\t{Fore.YELLOW}│{part['Size']}kb: {part['Value']}kb|")
+
+def fitWorst(parts, vals):
     attempts = 0
 
     toFill = []
-    while (len(toFill) < len(arr)):
-        for i in range(len(arr)):
-            if pos[i] <= arr[i] and not pos[i] in toFill:
-                toFill.append(pos[i])
-        attempt += 1
-        if attempt > 10:
-            print(f"{Fore.RED}Não há espaço disponível")
-            quit()
-
-    for i in range(len(arr)):
-        print(f"{Fore.YELLOW}{arr[i]}kb: {toFill[i]}kb")
-def swapWorst(arr, pos):
-    attempts = 0
-
-    toFill = []
-    arr.sort(reverse=True)
-    while (len(toFill) < len(arr)):
-        for i in range(len(arr)):
-            if pos[i] <= arr[i] and not pos[i] in toFill:
-                toFill.append(pos[i])
+    parts.sort(reverse=True)
+    while (len(toFill) < len(parts)):
+        for i in range(len(parts)):
+            if vals[i] <= parts[i] and not vals[i] in toFill:
+                toFill.append(vals[i])
         attempts += 1
         if attempts > 10:
             print(f"{Fore.RED}Não há espaço disponível")
             quit()
 
-    for i in range(len(arr)):
-        print(f"{Fore.YELLOW}{arr[i]}kb: {toFill[i]}kb")
+    for i in range(len(parts)):
+        print(f"{Fore.YELLOW}{parts[i]}kb: {toFill[i]}kb")
 
-def swapBest(arr, pos):
+
+def fitBest(parts, vals):
     attempts = 0
 
     toFill = []
-    arr.sort()
-    pos.sort()
-    while(len(toFill) < len(arr)):
-        for i in range(len(arr)):
-            if pos[i] <= arr[i] and not pos[i] in toFill:
-                toFill.append(pos[i])
+    parts.sort()
+    vals.sort()
+    while (len(toFill) < len(parts)):
+        for i in range(len(parts)):
+            if vals[i] <= parts[i] and not vals[i] in toFill:
+                toFill.append(vals[i])
         attempts += 1
         if attempts > 10:
             print(f"{Fore.RED}Não há espaço disponível")
             quit()
 
-    for i in range(len(arr)):
-        print(f"\t{Fore.YELLOW}│{arr[i]}kb: {toFill[i]}kb|")
+    for i in range(len(parts)):
+        print(f"\t{Fore.YELLOW}│{parts[i]}kb: {toFill[i]}kb|")
+
 
 if __name__ == '__main__':
     __main__()
